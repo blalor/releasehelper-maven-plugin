a3182c0ab0b3604c5a587c54409e04bbfab9e2cb
Brian Lalor <blalor@bravo5.org>
2012-10-02 06:36:03 -0400

    Improvements.  Can't currently build project from artifact.

--- src/main/java/org/bravo5/maven/plugins/releasehelper/ReleaseHelperMojo.java
+++ src/main/java/org/bravo5/maven/plugins/releasehelper/ReleaseHelperMojo.java
@@ -7,13 +7,20 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 
+import org.apache.maven.artifact.Artifact;
+
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.ProjectBuilder;
+import org.apache.maven.project.ProjectBuildingResult;
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.apache.maven.project.DefaultProjectBuildingRequest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
-import java.util.Map;
-import java.util.LinkedHashMap;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
 import java.io.IOException;
 
 @Mojo(name = "helper", requiresProject = true)
@@ -21,17 +28,22 @@ public class ReleaseHelperMojo extends AbstractMojo {
     @Component
     private MavenProject project;
 
+    @Component
+    private ProjectBuilder projectBuilder;
+
+    private ObjectMapper objectMapper;
+
+    public ReleaseHelperMojo() {
+        objectMapper = new ObjectMapper()
+            .enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
     // {{{ execute
     /** {@inheritDoc} */
     @Override
     public void execute() throws MojoExecutionException {
-        ObjectMapper om = new ObjectMapper()
-            .enable(SerializationFeature.INDENT_OUTPUT);
-
-        getLog().info("hello, world");
-
         try {
-            om.writeValue(System.out, projectToJson(project));
+            objectMapper.writeValue(System.out, projectToJson(project));
         } catch (IOException e) {
             throw new MojoExecutionException("IOE", e);
         }
@@ -39,20 +51,34 @@ public class ReleaseHelperMojo extends AbstractMojo {
     // }}}
 
     // {{{ projectToJson
-    private Map<String,Object> projectToJson(final MavenProject proj) {
-        Map<String,Object> json = new LinkedHashMap<String,Object>();
+    private ObjectNode projectToJson(final MavenProject proj) {
+        ObjectNode json = objectMapper.createObjectNode();
 
         MavenProject parent = proj.getParent();
 
         if (parent != null) {
             json.put("parent", projectToJson(parent));
         } else {
-            getLog().error("no parent?!");
+            getLog().info("no parent?!");
         }
 
         json.put("groupId", project.getGroupId());
         json.put("artifactId", project.getArtifactId());
         json.put("version", project.getVersion());
+
+        ArrayNode depArr = objectMapper.createArrayNode();
+        json.put("dependencies", depArr);
+
+        for (Artifact dep : project.getDependencyArtifacts()) {
+            ProjectBuildingRequest pbr = new DefaultProjectBuildingRequest();
+
+            try {
+                depArr.add(projectToJson(projectBuilder.build(dep, pbr).getProject()));
+            } catch (Exception e) {
+                throw new IllegalStateException("unable to build project", e);
+            }
+            // depArr.addPOJO(dep);
+        }
 
         return json;        
     }
