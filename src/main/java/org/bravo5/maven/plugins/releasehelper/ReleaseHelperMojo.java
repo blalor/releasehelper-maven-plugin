c73604e4579b2dc06f1355d945a500138b14289e
Brian Lalor <blalor@bravo5.org>
2012-10-02 11:28:52 -0400

    Ok, this sort of works, but not really…
    proj.getDependencyArtifacts() is often null when it shouldn't be.

--- src/main/java/org/bravo5/maven/plugins/releasehelper/ReleaseHelperMojo.java
+++ src/main/java/org/bravo5/maven/plugins/releasehelper/ReleaseHelperMojo.java
@@ -4,8 +4,11 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
+import org.apache.maven.execution.MavenSession;
+
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 
 import org.apache.maven.artifact.Artifact;
 
@@ -13,7 +16,7 @@ import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.ProjectBuilder;
 import org.apache.maven.project.ProjectBuildingResult;
 import org.apache.maven.project.ProjectBuildingRequest;
-import org.apache.maven.project.DefaultProjectBuildingRequest;
+import org.apache.maven.project.ProjectBuildingException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -21,18 +24,28 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
+import java.util.Set;
 import java.io.IOException;
 
-@Mojo(name = "helper", requiresProject = true)
+@Mojo(
+    name = "helper",
+    requiresProject = true,
+    requiresDependencyResolution = ResolutionScope.TEST
+)
 public class ReleaseHelperMojo extends AbstractMojo {
     @Component
-    private MavenProject project;
+    private MavenProject rootProject;
 
     @Component
     private ProjectBuilder projectBuilder;
 
-    private ObjectMapper objectMapper;
+    @Component
+    private MavenSession mavenSession;
 
+    private ObjectMapper objectMapper;
+    private ObjectNode jsonDeps;
+    private ProjectBuildingRequest pbr;
+    
     public ReleaseHelperMojo() {
         objectMapper = new ObjectMapper()
             .enable(SerializationFeature.INDENT_OUTPUT);
@@ -42,45 +55,80 @@ public class ReleaseHelperMojo extends AbstractMojo {
     /** {@inheritDoc} */
     @Override
     public void execute() throws MojoExecutionException {
+        pbr = mavenSession.getProjectBuildingRequest();
+        pbr.setResolveDependencies(true);
+        
+        jsonDeps = objectMapper.createObjectNode();
+        
+        visit(rootProject);
+        
         try {
-            objectMapper.writeValue(System.out, projectToJson(project));
+            objectMapper.writeValue(System.out, jsonDeps);
         } catch (IOException e) {
             throw new MojoExecutionException("IOE", e);
         }
     }
     // }}}
 
-    // {{{ projectToJson
-    private ObjectNode projectToJson(final MavenProject proj) {
-        ObjectNode json = objectMapper.createObjectNode();
-
-        MavenProject parent = proj.getParent();
-
-        if (parent != null) {
-            json.put("parent", projectToJson(parent));
-        } else {
-            getLog().info("no parent?!");
+    // {{{ visit
+    private void visit(final MavenProject proj) {
+        final String projKey = String.format("%s:%s", proj.getGroupId(), proj.getArtifactId());
+        
+        getLog().info("visiting project " + projKey);
+        
+        if (! proj.getArtifact().isSnapshot()) {
+            getLog().info(projKey + " is not snapshot");
+            return;
         }
-
-        json.put("groupId", project.getGroupId());
-        json.put("artifactId", project.getArtifactId());
-        json.put("version", project.getVersion());
-
-        ArrayNode depArr = objectMapper.createArrayNode();
-        json.put("dependencies", depArr);
-
-        for (Artifact dep : project.getDependencyArtifacts()) {
-            ProjectBuildingRequest pbr = new DefaultProjectBuildingRequest();
-
-            try {
-                depArr.add(projectToJson(projectBuilder.build(dep, pbr).getProject()));
-            } catch (Exception e) {
-                throw new IllegalStateException("unable to build project", e);
+        
+        if (jsonDeps.get(projKey) == null) {
+            ObjectNode jsonProj = jsonDeps.putObject(projKey);
+            
+            jsonProj.put("groupId", proj.getGroupId());
+            jsonProj.put("artifactId", proj.getArtifactId());
+            
+            MavenProject parent = proj.getParent();
+            if (parent == null) {
+                jsonProj.putNull("parent");
+            } else {
+                visit(parent);
+                
+                ObjectNode jsonParent = jsonProj.putObject("parent");
+                jsonParent.put("groupId", parent.getGroupId());
+                jsonParent.put("artifactId", parent.getArtifactId());
+                jsonParent.put("version", parent.getVersion());
             }
-            // depArr.addPOJO(dep);
+            
+            ArrayNode depArr = jsonProj.putArray("snapshotDependencies");
+            Set<Artifact> depArtifacts = proj.getDependencyArtifacts();
+            
+            if (depArtifacts == null) {
+                getLog().info("no dependency artifacts for " + projKey);
+            } else {
+                for (Artifact dep : depArtifacts) {
+                    if (dep.isSnapshot()) {
+                        final String depKey =
+                            String.format("%s:%s", dep.getGroupId(), dep.getArtifactId());
+                        
+                        getLog().info(String.format("%s -> %s", projKey, depKey));
+                        
+                        ObjectNode jsonDep = depArr.addObject();
+                        jsonDep.put("groupId", dep.getGroupId());
+                        jsonDep.put("artifactId", dep.getArtifactId());
+                        jsonDep.put("version", dep.getBaseVersion());
+                        
+                        try {
+                            // visit(dep -> MavenProject)
+                            visit(projectBuilder.build(dep, false, pbr).getProject());
+                        } catch (ProjectBuildingException e) {
+                            throw new IllegalStateException(e);
+                        }
+                    }
+                }
+            }
+        } else {
+            getLog().info("already visited " + projKey);
         }
-
-        return json;        
     }
     // }}}
 }
