20f5acfaaba6432c3bd51d531ad2f8adf0209995
Brian Lalor <blalor@bravo5.org>
2012-10-02 13:01:28 -0400

    Hey, it works!
    Had to switch back to Maven 2.0.9 because I could find working
    examples of how to resolve all the dependencies.

--- src/main/java/org/bravo5/maven/plugins/releasehelper/ReleaseHelperMojo.java
+++ src/main/java/org/bravo5/maven/plugins/releasehelper/ReleaseHelperMojo.java
@@ -1,22 +1,25 @@
 package org.bravo5.maven.plugins.releasehelper;
 
+// http://docs.codehaus.org/display/MAVENUSER/Mojo+Developer+Cookbook
+
+// http://maven.apache.org/ref/2.0.9/maven-artifact/apidocs/index.html
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.artifact.resolver.filter.ScopeArtifactFilter;
+
+// http://maven.apache.org/ref/2.0.9/maven-plugin-api/apidocs/index.html
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
-import org.apache.maven.execution.MavenSession;
-
-import org.apache.maven.plugins.annotations.Component;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.ResolutionScope;
-
-import org.apache.maven.artifact.Artifact;
-
+// http://maven.apache.org/ref/2.0.9/maven-project/apidocs/index.html
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.ProjectBuilder;
-import org.apache.maven.project.ProjectBuildingResult;
-import org.apache.maven.project.ProjectBuildingRequest;
+import org.apache.maven.project.MavenProjectBuilder;
 import org.apache.maven.project.ProjectBuildingException;
+import org.apache.maven.project.artifact.InvalidDependencyVersionException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -24,54 +27,114 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.List;
+
+import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-@Mojo(
-    name = "helper",
-    requiresProject = true,
-    requiresDependencyResolution = ResolutionScope.TEST
-)
+/**
+ * Generates JSON describing the -SNAPSHOT dependencies and parents in the
+ * current reactor.
+ *
+ * @goal helper
+ * @execute phase="validate"
+ * @aggregator
+ * @requiresDependencyResolution test
+ * @requiresProject true
+ */
 public class ReleaseHelperMojo extends AbstractMojo {
-    @Component
-    private MavenProject rootProject;
+    /**
+     * @parameter expression="${project}"
+     * @required
+     * @readonly
+     */
+    protected MavenProject rootProject;
 
-    @Component
-    private ProjectBuilder projectBuilder;
+    /**
+     * The projects in the reactor.
+     *
+     * @parameter expression="${reactorProjects}"
+     * @readonly
+     */
+    private List<MavenProject> reactorProjects;
 
-    @Component
-    private MavenSession mavenSession;
+    /**
+     * @parameter default-value="${localRepository}"
+     * @required
+     * @readonly
+     */
+    private ArtifactRepository localRepository;
 
-    private ObjectMapper objectMapper;
-    private ObjectNode jsonDeps;
-    private ProjectBuildingRequest pbr;
+    /**
+     * @component
+     * @required
+     * @readonly
+     */
+    private MavenProjectBuilder mavenProjectBuilder;
+
+    /**
+     * @component
+     * @required
+     * @readonly
+     */
+    private ArtifactFactory artifactFactory;
     
+    /**    
+     * @parameter expression="${outputFile}"
+     */
+    private File outputFile;
+
+    // Jackson JSON serializer
+    private ObjectMapper objectMapper;
+    
+    // the fruits of our efforts
+    private ObjectNode jsonDeps;
+    
+    // {{{ constructor
     public ReleaseHelperMojo() {
         objectMapper = new ObjectMapper()
             .enable(SerializationFeature.INDENT_OUTPUT);
     }
-
+    // }}}
+    
     // {{{ execute
     /** {@inheritDoc} */
     @Override
-    public void execute() throws MojoExecutionException {
-        pbr = mavenSession.getProjectBuildingRequest();
-        pbr.setResolveDependencies(true);
-        
+    public void execute() throws MojoExecutionException, MojoFailureException {
         jsonDeps = objectMapper.createObjectNode();
         
-        visit(rootProject);
-        
         try {
-            objectMapper.writeValue(System.out, jsonDeps);
+            // because @aggregator, walk all projects in the reactor
+            for (MavenProject p : reactorProjects) {
+                visit(p);
+            }
+
+            if (outputFile != null) {
+                objectMapper.writeValue(outputFile, jsonDeps);
+            } else {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                objectMapper.writeValue(baos, jsonDeps);
+                getLog().info(baos.toString());
+            }
+        } catch (InvalidDependencyVersionException e) {
+            getLog().error("invalid dependency version somewhere", e);
+        } catch (ProjectBuildingException e) {
+            getLog().error("unable to build a project somewhere", e);
         } catch (IOException e) {
             throw new MojoExecutionException("IOE", e);
         }
+        
+        jsonDeps = null; // just in case
     }
     // }}}
 
     // {{{ visit
-    private void visit(final MavenProject proj) {
+    private void visit(final MavenProject proj)
+        throws InvalidDependencyVersionException, ProjectBuildingException
+    {
         final String projKey = String.format("%s:%s", proj.getGroupId(), proj.getArtifactId());
         
         getLog().info("visiting project " + projKey);
@@ -93,15 +156,48 @@ public class ReleaseHelperMojo extends AbstractMojo {
             } else {
                 visit(parent);
                 
+                /*
+                "parent": {
+                    "groupId": "com.myco",
+                    "artifactId": "webservices",
+                    "version": "12.12.19-SNAPSHOT",
+                    "snapshot": true
+                }
+                */
                 ObjectNode jsonParent = jsonProj.putObject("parent");
                 jsonParent.put("groupId", parent.getGroupId());
                 jsonParent.put("artifactId", parent.getArtifactId());
                 jsonParent.put("version", parent.getVersion());
+                jsonParent.put("snapshot", parent.getArtifact().isSnapshot());
             }
             
-            ArrayNode depArr = jsonProj.putArray("snapshotDependencies");
-            Set<Artifact> depArtifacts = proj.getDependencyArtifacts();
+            // list of properties that look like they might be for SNAPSHOT
+            // versions
+            /*
+            "snapshotProperties": {
+                "contracts-version": "12.12.19-SNAPSHOT",
+                "properties-version": "12.12.19.1-SNAPSHOT"
+            }
+            */
+            ObjectNode jsonProps = jsonProj.putObject("snapshotProperties");
+            if (proj.getProperties() != null) {
+                for (Map.Entry<Object,Object> entry : proj.getProperties().entrySet()) {
+                    String key = (String) entry.getKey();
+                    String val = (String) entry.getValue();
+                    
+                    if (val.endsWith("-SNAPSHOT")) {
+                        jsonProps.put(key, val);
+                    }
+                }
+            }
             
+            /*
+            "snapshotDependencies": []
+            */            
+            ArrayNode depArr = jsonProj.putArray("snapshotDependencies");
+            
+            Set<Artifact> depArtifacts = proj.createArtifacts(artifactFactory, null, null);
+
             if (depArtifacts == null) {
                 getLog().info("no dependency artifacts for " + projKey);
             } else {
@@ -112,17 +208,26 @@ public class ReleaseHelperMojo extends AbstractMojo {
                         
                         getLog().info(String.format("%s -> %s", projKey, depKey));
                         
+                        /*
+                        {
+                            "groupId": "com.myco.commons",
+                            "artifactId": "service",
+                            "version": "12.7.13.1-SNAPSHOT"
+                        }
+                        */
                         ObjectNode jsonDep = depArr.addObject();
                         jsonDep.put("groupId", dep.getGroupId());
                         jsonDep.put("artifactId", dep.getArtifactId());
                         jsonDep.put("version", dep.getBaseVersion());
                         
-                        try {
-                            // visit(dep -> MavenProject)
-                            visit(projectBuilder.build(dep, false, pbr).getProject());
-                        } catch (ProjectBuildingException e) {
-                            throw new IllegalStateException(e);
-                        }
+                        visit(
+                            mavenProjectBuilder.buildFromRepository(
+                                dep,
+                                rootProject.getRemoteArtifactRepositories(),
+                                localRepository,
+                                false
+                            )
+                        );
                     }
                 }
             }
